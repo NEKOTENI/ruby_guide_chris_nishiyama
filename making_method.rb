02e03ad058cb1ccd876a3a5144403bfa68d76eac
NEKOTENI <riverfield.tk@gmail.com>
2014-11-14 18:13:41 +0900

    演算子の間にスペースを入れました

--- making_method.rb
+++ making_method.rb
@@ -1,6 +1,6 @@
 #上のenglishNumberを拡張してみましょう。 最初にthousand(千の桁)を導入してください。上のプログラムでは 'ten hundred'となっているところを'one thousand'を返すように、 あるいは、'one hundred hundred'の代わりに'ten thousand'を 返すようにします。
 
-def englishNumber number
+def englishNumber(number)
   if number < 0  #  負の数は不可です。
     return '負でない数を入力してください.'
   end
@@ -25,8 +25,8 @@ def englishNumber number
 
   remain = number
 
-  write = remain/1000          #  1000の桁以上の桁をwriteに入れます。
-  remain = remain - write*1000  #  残り3桁をremainに残します。
+  write = remain / 1000          #  1000の桁以上の桁をwriteに入れます。
+  remain = remain - write * 1000  #  残り3桁をremainに残します。
 
   if write > 0
 
@@ -40,8 +40,8 @@ def englishNumber number
     end
   end
 
-  write = remain/100
-  remain = remain - write*100
+  write = remain / 100
+  remain = remain - write * 100
 
   if write > 0
 
@@ -53,15 +53,15 @@ def englishNumber number
     end
   end
 
-  write = remain/10          #  今度は十の桁です。
-  remain = remain - write*10  #  そして十の桁をひきます。
+  write = remain / 10          #  今度は十の桁です。
+  remain = remain - write * 10  #  そして十の桁をひきます。
 
   if write > 0
     if ((write == 1) and (remain > 0))
-      numString = numString + teenagers[remain-1]
+      numString = numString + teenagers[remain - 1]
       remain = 0
     else
-      numString = numString + tensPlace[write-1]
+      numString = numString + tensPlace[write - 1]
     end
 
     if remain > 0
@@ -73,7 +73,7 @@ def englishNumber number
   remain = 0     #  この桁を引きます。(0になります。)
 
   if write > 0
-    numString = numString + onesPlace[write-1]
+    numString = numString + onesPlace[write - 1]
   end
 
   numString
